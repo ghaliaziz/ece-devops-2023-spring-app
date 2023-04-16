168475441b5429a816285ecc9aa57550fe7de013
WAED-DEV <98319519+WAED-DEV@users.noreply.github.com>
2023-04-16 12:38:14 +0200

    Add project files.

---
+++ Project/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Project/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Project.Data;
+using Project.Models;
+
+namespace Project.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Project.Data.ProjectContext _context;
+
+        public IndexModel(Project.Data.ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.User != null)
+            {
+                User = await _context.User.ToListAsync();
+            }
+        }
+    }
+}
